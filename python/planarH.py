4ad66286af203459730edf752debf08a2493b585
Lionel Derrick Roxas <lionelroxas80@gmail.com>
2025-10-14 13:08:31 -1000

    Implement AR video overlay and homography functions
    Added AR overlay script in ec/ar_ec.py to composite panda video frames onto book video using homography. Implemented computeH, computeH_norm, and computeH_ransac functions in python/planarH.py for homography estimation and normalization. Minor formatting update in python/q3_4.py.

--- python/planarH.py
+++ python/planarH.py
@@ -1,83 +1,9 @@
 import numpy as np
 import cv2
 
-
 def computeH(x1, x2):
-	#Q3.6
-	#Compute the homography between two sets of points
-
-
-
-
-	return H2to1
-
-
-def computeH_norm(x1, x2):
-	#Q3.7
-	#Compute the centroid of the points
-
-
-	#Shift the origin of the points to the centroid
-
-
-	#Normalize the points so that the largest distance from the origin is equal to sqrt(2)
-
-
-	#Similarity transform 1
-
-
-	#Similarity transform 2
-
-
-	#Compute homography
-
-
-	#Denormalization
-	
-
-	return H2to1
-
-
-
-
-def computeH_ransac(x1, x2):
-	#Q3.8
-	#Compute the best fitting homography given a list of matching points
-
-
-
-	return bestH2to1, inliers
-
-
-
-def compositeH(H2to1, template, img):
-	
-	#Create a composite image after warping the template image on top
-	#of the image using the homography
-
-	#Note that the homography we compute is from the image to the template;
-	#x_template = H2to1*x_photo
-	#For warping the template to the image, we need to invert it.
-	
-
-	#Create mask of same size as template
-
-	#Warp mask by appropriate homography
-
-	#Warp template by appropriate homography
-
-	#Use mask to combine the warped template and the image
-	
-	return composite_img
-
-
-import numpy as np
-import cv2
-
-
-def computeH(x1, x2):
-	#Q3.6
-	#Compute the homography between two sets of points
+	# Q3.6
+	# Compute the homography between two sets of points
 	num_points = x1.shape[0]
 	x_invt, y_invt = x1[0]
 	x, y = x2[0]
@@ -96,8 +22,8 @@ def computeH(x1, x2):
 	return H2to1
 
 def computeH_norm(x1, x2):
-	#Q3.7
-	#Compute the homography between two sets of points
+	# Q3.7
+	# Compute the homography between two sets of points
 	m1_x, m1_y = np.average(x1, axis=0)
 	m2_x, m2_y = np.average(x2, axis=0)
 
@@ -105,14 +31,12 @@ def computeH_norm(x1, x2):
 	x2_a = x2 - np.average(x2, axis=0)
 
 	# Normalize the points so that the largest distance from the origin is equal to sqrt(2)
-
 	dist_max1 = np.max(np.sqrt(np.sum(np.square(x1_a), axis=1)))
 	dist_max2 = np.max(np.sqrt(np.sum(np.square(x2_a), axis=1)))
 	x1_a = np.sqrt(2) / dist_max1 * x1_a
 	x2_a = np.sqrt(2) / dist_max2 * x2_a
 
 	# Similarity transform
-
 	T1 = np.sqrt(2) / dist_max1 * np.array([[1, 0, -m1_x], [0, 1, -m1_y], [0, 0, dist_max1 / np.sqrt(2)]])
 	T2 = np.sqrt(2) / dist_max2 * np.array([[1, 0, -m2_x], [0, 1, -m2_y], [0, 0, dist_max2 / np.sqrt(2)]])
 
@@ -123,48 +47,9 @@ def computeH_norm(x1, x2):
 	H2to1 = np.matmul(np.linalg.inv(T1), np.matmul(H2to1, T2))
 	return H2to1
 
-# def computeH_norm_(x1, x2):
-# 	#Q2.2.2
-# 	#Compute the centroid of the points
-# 	x1_cent = np.mean(x1, axis = 0)
-# 	x2_cent = np.mean(x2, axis = 0)
-# 	#Shift the origin of the points to the centroid
-# 	x1_shift = x1 - x1_cent
-# 	x2_shift = x2 - x2_cent
-# 	#Normalize the points so that the largest distance from the origin is equal to sqrt(2)
-# 	# x1_norm = x1_shift/np.linalg.norm(x1_shift, axis=-1)[:, np.newaxis]
-# 	# x2_norm = x2_shift/np.linalg.norm(x2_shift, axis=-1)[:, np.newaxis]
-# 	#Similarity transform 1
-# 	x1_ratio = np.reciprocal(np.sqrt(np.max(np.sum(x1_shift**2, axis=1))))*(np.sqrt(2))
-# 	x1_norm = x1_shift*x1_ratio
-# 	# ratio = np.reciprocal(np.sqrt(np.max(np.sum(x2**2))))*(np.)
-# 	T1 = np.zeros((3,3))
-# 	T1[0,0] = x1_ratio
-# 	T1[0,2] = -x1_cent[0]*x1_ratio
-# 	T1[1,1] = x1_ratio
-# 	T1[1,2] = -x1_cent[1]*x1_ratio
-# 	T1[2,2] = 1
-#
-# 	#Similarity transform 2
-# 	x2_ratio = np.reciprocal(np.sqrt(np.max(np.sum(x2_shift**2, axis=1)))) * (np.sqrt(2))
-# 	x2_norm = x2_shift*x2_ratio
-# 	T2 = np.zeros((3,3))
-# 	T2[0,0] = x2_ratio
-# 	T2[0,2] = -x2_cent[0]*x2_ratio
-# 	T2[1,1] = x2_ratio
-# 	T2[1,2] = -x2_cent[1]*x2_ratio
-# 	T2[2,2] = 1
-#
-# 	#Compute homography
-# 	H_homo = computeH_(x1_norm, x2_norm)
-#
-# 	#Denormalization
-# 	T1_inv = np.linalg.inv(T1)
-# 	H2to1 = T1_inv@H_homo@T2
-#
-# 	return H2to1
-
 def computeH_ransac(locs1, locs2):
+	# Q3.8
+	# Write a function: bestH2to1, inliers = computeH ransac(x1, x2) where bestH2to1 should be the homography H with most inliers found during RANSAC.
 	num_points = locs1.shape[0]
 	mat_one_all = np.ones((locs2.shape[0], 1))
 
@@ -202,50 +87,6 @@ def computeH_ransac(locs1, locs2):
 			bestH2to1 = H2to1
 
 	return bestH2to1, inliers
-
-
-# def computeH_ransac_(locs1, locs2):
-# 	#Q2.2.3
-# 	#Compute the best fitting homography given a list of matching points
-# 	np.random.seed()
-# 	iteration_num = 500
-# 	threshold = 10
-# 	max_inlier = 0
-#
-# 	for iter in range(iteration_num):
-# 		idx = np.random.choice(locs1.shape[0], 4, replace = False)
-# 		p1 = locs1[idx]
-# 		p2 = locs2[idx]
-#
-# 		H = computeH_(p1, p2)
-#
-# 		locs1_homo = np.vstack((np.transpose(locs1), np.ones((1, locs1.shape[0]))))
-# 		locs2_homo = np.vstack((np.transpose(locs2), np.ones((1, locs2.shape[0]))))
-#
-# 		repro = np.matmul(H, locs2_homo)
-# 		repro_norm = np.divide(repro, repro[2,:])
-#
-# 		error = locs1_homo - repro_norm
-#
-# 		# inliers = np.zeros((locs1.shape[0]))
-# 		# for i in range(locs1.shape[0]):
-# 		# 	dist = np.sqrt(np.sum(error**2, axis=0))
-# 		# 	if dist <= threshold:
-# 		# 		inliers[i] = 1
-# 		dist = np.sqrt(np.sum(error**2, axis=0))
-# 		inliers_ = [1 if x < threshold else 0 for x in dist]
-#
-#
-# 		inliers_num = np.sum(inliers_)
-# 		if max_inlier < inliers_num:
-# 			max_inlier = inliers_num
-# 			bestH2to1 = H
-# 			inliers = inliers_
-#
-#
-# 	return bestH2to1, inliers
-
-
 
 def compositeH(H2to1, template, img):
 	
